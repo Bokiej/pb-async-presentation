f4e1d75b033e8516f3c395c76c38b1c415f0f68c
Piotr Bokiej <pbokiej@sii.pl>
2022-05-26 17:54:24 +0200

    updated bulb component

--- force-app/main/default/lwc/bulb/bulb.js
+++ force-app/main/default/lwc/bulb/bulb.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Bulb extends LightningElement {}
+export default class Bulb extends LightningElement {
+    @api color;
+    @api number;
+    @api time;
+
+    startTime;
+
+    connectedCallback() {
+        this.startTime = new Date().getTime();
+        this.setBulbColor(this.color);
+    }
+
+    @api
+    setBulbColor(value) {
+        if (!!value) {
+            this.color = value;
+            this.template.host.style?.setProperty('--pb-bulb-bg-color', value);
+        }
+    }
+
+    @api
+    setBulbTime() {
+        this.time = (new Date().getTime() - this.startTime) / 1000;
+    }
+}
