e3e90b604d48d2f04805371180bfc7e4c6f427da
Piotr Bokiej <pbokiej@sii.pl>
2022-06-24 20:06:42 +0200

    [basic] created utility component

---
+++ force-app/main/default/lwc/utility/utility.js
@@ -0,0 +1,28 @@
+function lightBulb({ name, color }) {
+    return new Promise((resolve, reject) => {
+        if (!!name || !!color) {
+            setTimeout(
+                () => resolve({ name, color }),
+                setRandomTimeout()
+            );
+        } else {
+            reject('color or name value is undefined');
+        }
+    });
+}
+
+function setRandomTimeout(min = 1, max = 5) {
+    return (Math.floor(Math.random() * (max - min)) + min) * 1000;
+}
+
+export default lightBulb;
+
+function showError({ context, error }) {
+    context.dispatchEvent(new CustomEvent('notify', {
+        detail: { type: 'error', error },
+        bubbles: true,
+        composed: true
+    }));
+}
+
+export { showError };
