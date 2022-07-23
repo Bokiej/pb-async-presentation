918d9edd0dc952118980463563598b84e20c402d
Piotr Bokiej <pbokiej@sii.pl>
2022-07-23 20:39:23 +0200

    updated basic code

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const open = require('open');
 const path = require('path');
 
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8000;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 app.listen(port, async () => {
     console.log(`Server running on port ${port}`);
     await open('http://localhost:8000');
@@ -16,15 +18,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 });
 
-app.get('/next-branch', (req, res) => {
-    exec('git checkout -', (error, stdout, stderr) => {
+app.post('/next-branch', (req, res) => {
+    const { branch } = req.body;
+
+    exec(`git stash && git checkout ${branch}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`error: ${error.message}`);
             return res.status(500).send(error.message);
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return res.status(500).send(stderr);
         }
         res.send(stdout);
     });
