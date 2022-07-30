1d9bfaf893623f3c844dc0bfc7ba86431a24bace
Piotr Bokiej <pbokiej@sii.pl>
2022-07-30 20:13:14 +0200

    [async-3-2-1] updated bulbsContainer

--- force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
+++ force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
@@ -6,25 +6,22 @@ import lightBulb from 'c/utility'; // here goes Apex callback
 export default class BulbsContainer extends LightningElement {
 
     connectedCallback() {
-        lightBulb({ name: 'yellow', color: '#FFEE58' }).then(data =>
-            // destructuring, adding name and color values
-            // in place of ...data
-            setBulbColor({ context: this, ...data })
-        ).then(bulb => {
-            // if you use curly braces remember to use return
-            // statement for returned value
+        lightBulb({ name: 'yellow', color: '#FFEE58' }).then(data => {
+            console.log('1. setBulbColor');
+            return setBulbColor({ context: this, ...data });
+         } ).then(bulb => {
+            console.log('2. setBulbTime');
             return setBulbTime(bulb);
-        }).then(bulb =>
-            // but if you removed curly braces statement will be 
-            // automatically returned
-            // in such case do not use ; at the end
-            setBulbCounter(bulb)
-        ).then(bulb =>
-            showBulbNumber(bulb)
-        ).catch(error => {
+        }).then(bulb => {
+            console.log('3. setBulbCounter');
+            return setBulbCounter(bulb);
+        }).then(bulb => {
+            console.log('4. showBulbNumber');
+            return showBulbNumber(bulb);
+        }).catch(error => {
             showError({ context: this, error });
         }).finally(() => {
-            console.log('everything is done');
+            console.warn('finally method');
         });
     }
 }
