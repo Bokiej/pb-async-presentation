a65a559c23ea5440f5d82a1254674044f7c3dc33
Piotr Bokiej <pbokiej@sii.pl>
2022-08-04 14:21:01 +0200

    [async-4-6-1] updated bulbsContainer

--- force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
+++ force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
@@ -4,31 +4,40 @@ import { showError } from 'c/utility';
 import lightBulb from 'c/utility'; // here goes Apex callback
 
 export default class BulbsContainer extends LightningElement {
+    isLoading = true;
+
     YELLOW = { name: 'yellow', color: '#FFEE58' };
     RED = { name: 'red', color: '#FF7043' };
     GREEN = { name: 'green', color: '#66BB6A' };
     BLUE = { name: 'blue', color: '#4FC3F7' }; 
 
     connectedCallback() {
-        Promise.any([
-            lightBulb({ ...this.YELLOW }),
-            lightBulb({ ...this.RED }).then(() => {
-                throw new Error('Red bulb Error');
-            }),
-            lightBulb({ ...this.GREEN }),
-            lightBulb({ ...this.BLUE }).then(() => {
-                throw new Error('Blue bulb Error');
-            })
-        ]).then(bulb =>
+        this.runBulb(
+            lightBulb(this.YELLOW)
+        ).then(() =>
+            this.runBulb(lightBulb(this.RED))
+        ).then(() =>
+            this.runBulb(lightBulb(this.GREEN))
+        ).then(() =>
+            this.runBulb(lightBulb(this.BLUE))
+        ).catch(error => {
+            showError({ context: this, error });
+        }).finally(() => {
+            this.isLoading = false;
+        });
+    }
+
+    runBulb(promise) {
+        return promise.then(bulb =>
             setBulbColor({ context: this, ...bulb })
         ).then(bulb =>
             setBulbTime(bulb)
         ).then(bulb =>
             setBulbCounter(bulb)
-        ).then(bulb => {
+        ).then(bulb =>
             showBulbNumber(bulb)
-        }).catch(error => {
-            showError({ context: this, error });
+        ).then(bulb => {
+            console.log(`'${bulb.dataset.name}' bulb works`)
         });
     }
 }
