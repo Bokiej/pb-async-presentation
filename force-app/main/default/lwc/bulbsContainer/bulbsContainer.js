a634240ca1ad56ad22bb9a5271a29326c5c7066d
Piotr Bokiej <pbokiej@sii.pl>
2022-08-05 12:44:48 +0200

    [async-6-1-1] updated bulbsContainer

--- force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
+++ force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
@@ -11,8 +11,17 @@ export default class BulbsContainer extends LightningElement {
     BLUE = { name: 'blue', color: '#4FC3F7' };
     WIRE = { name: 'wire' };
 
+    wireBulbResolved = false;
+    otherBulbsResolved = false;
+
+    isRendered = false;
+
+    get isLoading() {
+        return !(this.wireBulbResolved && this.otherBulbsResolved);
+    }
+
     @wire(getBulb, {})
-    test({error, data}) {
+    getBulb({error, data}) {
         if (data) {
             this.wire = JSON.parse(data);
 
@@ -20,7 +29,9 @@ export default class BulbsContainer extends LightningElement {
 
             setBulbTime(bulb);
             setBulbCounter(bulb);
-            showBulbNumber(bulb);
+
+            this.wireBulbResolved = true;
+            console.log(`'${this.wire.name}' bulb is running`);
         } else if (error) {
             showError({ context: this, error });
         }
@@ -38,10 +49,20 @@ export default class BulbsContainer extends LightningElement {
             bulbs.map(bulb => setBulbTime(bulb))
         ).then(bulbs =>
             bulbs.map(bulb => setBulbCounter(bulb))
-        ).then(bulbs =>
-            bulbs.map(bulb => showBulbNumber(bulb))
         ).catch(error => {
             showError({ context: this, error });
+        }).finally(() => {
+            this.otherBulbsResolved = true;
+            console.log(`other bulbs are running`);
         });
+    }
+
+    renderedCallback() {
+        if (!this.isLoading && !this.isRendered) {
+            setTimeout(() => {
+                showAllBulbNumbers({ context: this });
+                this.isRendered = true;
+            }, 2000);
+        }
     }
 }
