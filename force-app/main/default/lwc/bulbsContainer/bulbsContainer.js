6b68897a70849c10e09e8c3c45b99e507b7682b4
Piotr Bokiej <pbokiej@sii.pl>
2022-08-05 17:30:49 +0200

    [async-6-3-1] updated bulbsContainer

--- force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
+++ force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
@@ -1,7 +1,6 @@
-import { LightningElement, wire } from 'lwc';
-import { setBulbColor, setBulbTime, setBulbCounter, showBulbNumber, showAllBulbNumbers } from './js/bulbUtilities';
+import { LightningElement } from 'lwc';
+import { setBulbColor, setBulbTime, setBulbCounter, showBulbNumber, showAllBulbNumbers, clearOrder } from './js/bulbUtilities';
 import { showError } from 'c/utility';
-import getBulb from '@salesforce/apex/bulbController.getBulb'; // real Apex callback for wire
 import lightBulb from 'c/utility'; // here goes Apex callback
 
 export default class BulbsContainer extends LightningElement {
@@ -9,60 +8,53 @@ export default class BulbsContainer extends LightningElement {
     RED = { name: 'red', color: '#FF7043' };
     GREEN = { name: 'green', color: '#66BB6A' };
     BLUE = { name: 'blue', color: '#4FC3F7' };
-    WIRE = { name: 'wire' };
 
-    wireBulbResolved = false;
-    otherBulbsResolved = false;
+    allBulbs = [ this.YELLOW, this.RED, this.GREEN, this.BLUE ];
+    bulbsToRender = [];
 
-    isRendered = false;
+    promiseId = 0; // to check what promises currently is the last one
 
-    get isLoading() {
-        return !(this.wireBulbResolved && this.otherBulbsResolved);
-    }
+    // CHECKBOX VARIABLES
+    choosedBulbs = [];
 
-    @wire(getBulb, {})
-    getBulb({error, data}) {
-        if (data) {
-            this.wire = JSON.parse(data);
-
-            const bulb = setBulbColor({ context: this, ...this.wire });
-
-            setBulbTime(bulb);
-            setBulbCounter(bulb);
-
-            this.wireBulbResolved = true;
-            console.log(`'${this.wire.name}' bulb is running`);
-        } else if (error) {
-            showError({ context: this, error });
-        }
-    }
-
-    connectedCallback() {
-        Promise.all([
-            lightBulb({ ...this.YELLOW }),
-            lightBulb({ ...this.RED }),
-            lightBulb({ ...this.GREEN }),
-            lightBulb({ ...this.BLUE })
-        ]).then(bulbs =>
-            bulbs.map(bulb => setBulbColor({ context: this, ...bulb }))
-        ).then(bulbs =>
-            bulbs.map(bulb => setBulbTime(bulb))
-        ).then(bulbs =>
-            bulbs.map(bulb => setBulbCounter(bulb))
-        ).catch(error => {
-            showError({ context: this, error });
-        }).finally(() => {
-            this.otherBulbsResolved = true;
-            console.log(`other bulbs are running`);
+    get options() {
+        return this.allBulbs.map(el => {
+            return { label: el.name, value: el.name };
         });
     }
 
-    renderedCallback() {
-        if (!this.isLoading && !this.isRendered) {
-            setTimeout(() => {
-                showAllBulbNumbers({ context: this });
-                this.isRendered = true;
-            }, 2000);
-        }
+    handleBulbsChange(event) {
+        // clear all rendered bulbs
+        this.bulbsToRender = [];
+        this.choosedBulbs = event.detail.value;
+        
+        clearOrder();
+
+        // wait to clear HTML
+        setTimeout(() => {
+            const currentPromiseId = ++this.promiseId;
+
+            this.bulbsToRender = this.allBulbs.filter(el => this.choosedBulbs.includes(el.name));
+
+            Promise.all( this.bulbsToRender.map(
+                bulb => lightBulb({ ...bulb }))
+            ).then(bulbs => {
+                // check if current promise is resolved not previous one
+                if (currentPromiseId === this.promiseId) {
+                    return Promise.resolve(bulbs)
+                        .then(bulbs =>
+                            bulbs.map(bulb => setBulbColor({ context: this, ...bulb }))
+                        ).then(bulbs =>
+                            bulbs.map(bulb => setBulbTime(bulb))
+                        ).then(bulbs =>
+                            bulbs.map(bulb => setBulbCounter(bulb))
+                        ).then(bulbs =>
+                            bulbs.map(bulb => showBulbNumber(bulb))
+                        );
+                }
+            }).catch(error => {
+                showError({ context: this, error });
+            });
+        }, 0);
     }
 }
