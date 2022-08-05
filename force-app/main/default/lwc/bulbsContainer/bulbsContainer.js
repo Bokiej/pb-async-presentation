ebf251d67581459a81a38fd5cdd38314f9ae0619
Piotr Bokiej <pbokiej@sii.pl>
2022-08-05 13:33:00 +0200

    [async-6-2-1] updated bulbsContainer

--- force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
+++ force-app/main/default/lwc/bulbsContainer/bulbsContainer.js
@@ -1,68 +1,28 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import { setBulbColor, setBulbTime, setBulbCounter, showBulbNumber, showAllBulbNumbers } from './js/bulbUtilities';
 import { showError } from 'c/utility';
-import getBulb from '@salesforce/apex/bulbController.getBulb'; // real Apex callback for wire
 import lightBulb from 'c/utility'; // here goes Apex callback
 
 export default class BulbsContainer extends LightningElement {
     YELLOW = { name: 'yellow', color: '#FFEE58' };
-    RED = { name: 'red', color: '#FF7043' };
-    GREEN = { name: 'green', color: '#66BB6A' };
-    BLUE = { name: 'blue', color: '#4FC3F7' };
-    WIRE = { name: 'wire' };
-
-    wireBulbResolved = false;
-    otherBulbsResolved = false;
-
-    isRendered = false;
-
-    get isLoading() {
-        return !(this.wireBulbResolved && this.otherBulbsResolved);
-    }
-
-    @wire(getBulb, {})
-    getBulb({error, data}) {
-        if (data) {
-            this.wire = JSON.parse(data);
-
-            const bulb = setBulbColor({ context: this, ...this.wire });
-
-            setBulbTime(bulb);
-            setBulbCounter(bulb);
-
-            this.wireBulbResolved = true;
-            console.log(`'${this.wire.name}' bulb is running`);
-        } else if (error) {
-            showError({ context: this, error });
-        }
-    }
 
     connectedCallback() {
-        Promise.all([
-            lightBulb({ ...this.YELLOW }),
-            lightBulb({ ...this.RED }),
-            lightBulb({ ...this.GREEN }),
-            lightBulb({ ...this.BLUE })
-        ]).then(bulbs =>
-            bulbs.map(bulb => setBulbColor({ context: this, ...bulb }))
-        ).then(bulbs =>
-            bulbs.map(bulb => setBulbTime(bulb))
-        ).then(bulbs =>
-            bulbs.map(bulb => setBulbCounter(bulb))
-        ).catch(error => {
-            showError({ context: this, error });
-        }).finally(() => {
-            this.otherBulbsResolved = true;
-            console.log(`other bulbs are running`);
-        });
-    }
+        lightBulb({ ...this.YELLOW }).then(bulb =>
+            setBulbColor({ context: this, ...bulb })
+        ).then(bulb =>
+           setBulbTime(bulb)
+        ).then(bulb =>
+            setBulbCounter(bulb)
+        ).then(bulb => {
+            showBulbNumber(bulb);
 
-    renderedCallback() {
-        if (!this.isLoading && !this.isRendered) {
-            setTimeout(() => {
-                showAllBulbNumbers({ context: this });
-                this.isRendered = true;
-            }, 2000);
-        }
+            const number = this.template
+                .querySelector(`c-bulb[data-name=${this.YELLOW.name}]`)
+                .getBulbNumber();
+    
+            console.log(`number of '${this.YELLOW.name}' bulb is: ${number}`);
+        }).then(bulb => {}).catch(error => {
+            showError({ context: this, error });
+        });
     }
 }
