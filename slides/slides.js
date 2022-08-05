62c4cf95ac4b70164feebfbfd530609a035ba19d
Piotr Bokiej <pbokiej@sii.pl>
2022-08-05 18:47:31 +0200

    [async-6-4-1] updated slides

--- slides/slides.js
+++ slides/slides.js
@@ -1,14 +1,20 @@
 const body = fetch('./slides/slides.html');
+const utilityJs = fetch('./force-app/main/default/lwc/utility/utility.js');
+const bulbJs = fetch('./force-app/main/default/lwc/bulb/bulb.js');
 
 /*
  * Init method
  */
-Promise.all([body]).then(([body]) => {
-    return Promise.all([body.text()]);
-}).then(([text]) => {
+Promise.all([body, utilityJs, bulbJs]).then(([body, utilityJs, bulbJs]) => {
+    return Promise.all([body.text(), utilityJs?.text(), bulbJs?.text()]);
+}).then(([text, utilityJs, bulbJs]) => {
 	const html = new DOMParser().parseFromString(text, 'text/html');
     const bodyEl = document.querySelector('body');
+    const utilityJsFullArray = [ ...html.querySelectorAll('.utility-full') ];
+    const bulbJsFullArray = [ ...html.querySelectorAll('.bulb-js-full') ];
 
+    utilityJsFullArray.forEach(el => el.innerHTML = utilityJs);
+    bulbJsFullArray.forEach(el => el.innerHTML = bulbJs);
     bodyEl.innerHTML = html.body.innerHTML + bodyEl.innerHTML;
 }).then(() => {
     // Reveal imported in index.html
